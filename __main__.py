e4a71b6a614efc4aec740a0e2894f0591ba06a89
jarbasai <jarbasai@mailfence.com>
2023-02-08 13:20:33 +0000

    refactor/move_gui_from_core

--- __main__.py
+++ __main__.py
@@ -1,4 +1,4 @@
-from mycroft.gui.__main__ import main
+from ovos_gui.__main__ import main
 
 if __name__ == "__main__":
     main()
---
+++ ovos_gui/service.py
@@ -0,0 +1,68 @@
+from mycroft_bus_client import MessageBusClient
+from ovos_utils.log import LOG
+from ovos_utils.process_utils import ProcessStatus, StatusCallbackMap, ProcessState
+
+from ovos_gui.extensions import ExtensionsManager
+from ovos_gui.namespace import NamespaceManager
+
+
+def on_started():
+    LOG.info('Gui Service is starting up.')
+
+
+def on_alive():
+    LOG.info('Gui Service is alive.')
+
+
+def on_ready():
+    LOG.info('Gui Service is ready.')
+
+
+def on_error(e='Unknown'):
+    LOG.info(f'Gui Service failed to launch ({e})')
+
+
+def on_stopping():
+    LOG.info('Gui Service is shutting down...')
+
+
+class GUIService:
+    def __init__(self, alive_hook=on_alive, started_hook=on_started, ready_hook=on_ready,
+                 error_hook=on_error, stopping_hook=on_stopping):
+        self.bus = MessageBusClient()
+        self.extension_manager = None
+        self.gui = NamespaceManager(self.bus)
+        callbacks = StatusCallbackMap(on_started=started_hook,
+                                      on_alive=alive_hook,
+                                      on_ready=ready_hook,
+                                      on_error=error_hook,
+                                      on_stopping=stopping_hook)
+        self.status = ProcessStatus('gui_service', callback_map=callbacks)
+        self.status.bind(self.bus)
+
+    def _init_bus_client(self):
+        """Start the bus client daemon and wait for connection."""
+        # Wait for connection
+        Configuration.set_config_update_handlers(self.bus)
+        if not self.bus.connected_event.is_set():
+            self.bus.run_in_thread()
+        self.bus.connected_event.wait()
+        LOG.info('Connected to messagebus')
+
+    def run(self):
+        """Start the GUI after it has been constructed."""
+        # Allow exceptions to be raised to the GUI Service
+        # if they may cause the Service to fail.
+        self.status.set_alive()
+        self._init_bus_client()
+        self.extension_manager = ExtensionsManager(
+            "EXTENSION_SERVICE", self.bus, self.gui)
+        self.status.set_ready()
+
+    def is_alive(self):
+        """Respond to is_alive status request."""
+        return self.status.state >= ProcessState.ALIVE
+
+    def stop(self):
+        """Perform any GUI shutdown processes."""
+        self.status.set_stopping()
