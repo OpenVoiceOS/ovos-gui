9cc1db7d4cf283161c5b3b9c1c089e7dd4b72eac
JarbasAI <33701864+JarbasAl@users.noreply.github.com>
2023-03-03 01:30:46 +0000

    refactor/move_gui_from_core (#1)
    * refactor/move_gui_from_core
    
    * pkg

---
+++ ovos_gui/__main__.py
@@ -0,0 +1,36 @@
+from ovos_config.locale import setup_locale
+from ovos_utils import wait_for_exit_signal
+from ovos_utils.log import LOG, init_service_logger
+
+from ovos_gui.service import GUIService
+
+
+def on_ready():
+    LOG.info("GUI websocket started!")
+
+
+def on_stopping():
+    LOG.info('GUI websocket is shutting down...')
+
+
+def on_error(e='Unknown'):
+    LOG.error(f'GUI websocket failed: {repr(e)}')
+
+
+def main(ready_hook=on_ready, error_hook=on_error, stopping_hook=on_stopping):
+    init_service_logger("gui")
+    LOG.debug("GUI websocket created")
+    try:
+        setup_locale()
+        service = GUIService()
+        service.run()
+        ready_hook()
+        wait_for_exit_signal()
+        service.stop()
+        stopping_hook()
+    except Exception as e:
+        error_hook(e)
+
+
+if __name__ == "__main__":
+    main()
