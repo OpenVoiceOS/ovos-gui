1407aa9649824899d9670e78cbcf3cea0f7ea064
JarbasAi <jarbasai@mailfence.com>
2023-05-02 03:36:04 +0100

    not bytes

--- ovos_gui/page.py
+++ ovos_gui/page.py
@@ -7,7 +7,7 @@ from ovos_utils.log import LOG
 class GuiPage:
     qml_server = None
 
-    def __init__(self, url: str, name: str, persistent: bool, duration: int, contents: bytes = None):
+    def __init__(self, url: str, name: str, persistent: bool, duration: int, contents: str = None):
         """
         A GuiPage represents a single GUI Display within a given namespace.
         A Page can either be `persistent` or be removed after some `duration`.
@@ -24,7 +24,7 @@ class GuiPage:
         if not url.startswith("http"):
             fname = md5(url.encode("utf-8")).hexdigest() + ".qml"
             if not isfile(url) and contents:
-                with open(url, "wb") as f:
+                with open(url, "w") as f:
                     f.write(contents)
             if self.qml_server is not None:
                 dst = self.qml_server.qml_path + "/" + fname
