4689697db58a0a1a33c40cc125ec674353abd9d3
Daniel McKnight <daniel@neon.ai>
2023-09-28 15:32:12 -0700

    Check skill service status on init Closes #28

--- ovos_gui/namespace.py
+++ ovos_gui/namespace.py
@@ -463,6 +463,12 @@ class NamespaceManager:
         self._init_gui_server()
         self._define_message_handlers()
 
+        resp = self.core_bus.wait_for_response(Message("mycroft.skills.is_ready",
+                                                       context={"source": "gui", "destination": ["skills"]}))
+        if resp and resp.data.get("status"):
+            LOG.debug("Skills service already running")
+            self.handle_ready()
+
     @property
     def _active_homescreen(self) -> str:
         return Configuration().get('gui', {}).get('idle_display_skill')
@@ -495,7 +501,7 @@ class NamespaceManager:
         self.core_bus.on("gui.page_gained_focus", self.handle_page_gained_focus)
         self.core_bus.on("mycroft.skills.trained", self.handle_ready)
 
-    def handle_ready(self, message):
+    def handle_ready(self, message=None):
         self._ready_event.set()
         self.core_bus.on("gui.volunteer_page_upload",
                          self.handle_gui_pages_available)
